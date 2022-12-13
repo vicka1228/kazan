f4275664c90cdec0bebea3187e95d64f71522f49
vicka1228 <bm3001@nyu.edu>
2022-12-12 19:37:02 -0500

    added fronted axios requests

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,6 +6,8 @@ import axios from 'axios'
 import { routes } from "./router/routes";
 import React from "react";
 function App() {
+
+  //test API
   const [getMessage, setGetMessage] = useState({})
 
   useEffect(()=>{
@@ -17,6 +19,88 @@ function App() {
     })
 
   }, [])
+
+  //Concerts List API
+
+  const [getConcertsList, setGetConcertsList] = useState({})
+
+  useEffect(()=>{
+    axios.get('http://127.0.0.1:5000/flask/concerts').then(response => {
+      console.log("SUCCESS", response)
+      setGetConcertsList(response)
+    }).catch(error => {
+      console.log(error)
+    })
+
+  }, [])
+
+  //Artists List API
+
+  const [getArtistsList, setGetArtistsList] = useState({})
+
+  useEffect(()=>{
+    axios.get('http://127.0.0.1:5000/flask/artists').then(response => {
+      console.log("SUCCESS", response)
+      setGetArtistsList(response)
+    }).catch(error => {
+      console.log(error)
+    })
+
+  }, [])
+
+  //Concert Info API
+
+  const [getConcertInfo, setGetConcertInfo] = useState({})
+  venue = "" //have to accept from user
+  req = {
+    'venue': venue
+  }
+  useEffect(()=>{
+    axios.post('http://127.0.0.1:5000/flask/concertinfo', req).then(response => {
+      console.log("SUCCESS", response)
+      setGetConcertInfo(response)
+    }).catch(error => {
+      console.log(error)
+    })
+
+  }, [])
+
+  //Search API
+
+  const [getSearchResult, setGetSearchResult] = useState({})
+  searchString = "" //have to accept from user
+  req = {
+    'search-string': searchString
+  }
+  useEffect(()=>{
+    axios.post('http://127.0.0.1:5000/flask/search', req).then(response => {
+      console.log("SUCCESS", response)
+      setGetSearchResult(response)
+    }).catch(error => {
+      console.log(error)
+    })
+
+  }, [])
+
+
+  //Concerts of Artist API
+
+  const [getConcertsOfArtist, setGetConcertsofArtist] = useState({})
+  artist = "" //have to accept from user
+  req = {
+    'artist': artist
+  }
+  useEffect(()=>{
+    axios.post('http://127.0.0.1:5000/flask/artistconcerts', req).then(response => {
+      console.log("SUCCESS", response)
+      setGetConcertsofArtist(response)
+    }).catch(error => {
+      console.log(error)
+    })
+
+  }, [])
+
+
 
 
   return (
