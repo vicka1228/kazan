8c7179e0cba09d0006d01b4eb01ae578569f219a
priyamvadadaga <psd7123@nyu.edu>
2022-12-13 13:11:13 -0500

    Update concert-data.py

--- back-end/concert-data.py
+++ back-end/concert-data.py
@@ -1,3 +1,4 @@
+from datacollection import getPopularity, getPopulation
 import requests
 from bs4 import BeautifulSoup
 import requests
@@ -64,3 +65,34 @@ def getConcerts(artist):
             continue
         events.append(show)    
     return events
+
+def MLDataPoints(eventId):
+    apikey = '0oiXuNOurAs4uGkbcByIC8bWWRZ8DhbN'
+    url = 'https://app.ticketmaster.com/discovery/v2/events/{}?apikey={}'.format(eventId, apikey)
+    jsonresponse = requests.get(url).json()
+
+    if not '_embedded' in jsonresponse:
+        return show
+
+    d8 = jsonresponse['dates']['start']['localDate']
+    year, month, day = "", "", ""
+    for j in range(0, len(d8)):
+        if j >= 0 and j <= 3:
+            year += d8[j]
+        elif j >= 5 and j <= 6:
+            month += d8[j]
+        elif j > 7 and j <= 9:
+            day += d8[j]
+    date = datetime.date(int(year), int(month), int(day))
+    eventday = date.weekday()
+
+    # 'weekend', 'score', 'month', 'pop', 'genre', 'artist', 'venue'
+    weekend = 1 if (eventday == 5 or eventday == 6) else 0
+    artist  = jsonresponse['_embedded']['attractions'][0]['name']
+    score   = getPopularity(artist)
+    venue   = jsonresponse['_embedded']['venues'][0]['name']
+    city    = jsonresponse['_embedded']['venues'][0]['city']['name']
+    pop     = getPopulation(city)
+    genre   = jsonresponse['_embedded']['attractions'][0]['classifications'][0]['genre']['name']
+
+    return [weekend, score, month, pop, genre, artist, venue]
